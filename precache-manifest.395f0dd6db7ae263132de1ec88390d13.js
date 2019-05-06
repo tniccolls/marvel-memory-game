6d2ec838e5e02950ce3e360084009c4e399ea3a4
Tyler Niccolls <tylerjniccolls@gmail.com>
2019-05-06 14:06:53 -0700

    Updates

---
+++ precache-manifest.395f0dd6db7ae263132de1ec88390d13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68c3900559a210d48b5b18eb62adcd3b",
+    "url": "/marvel-memory-game/index.html"
+  },
+  {
+    "revision": "1f021caa5070b7caa62c",
+    "url": "/marvel-memory-game/static/css/main.a17c0fe0.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/marvel-memory-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "1f021caa5070b7caa62c",
+    "url": "/marvel-memory-game/static/js/main.b7d1ee7a.chunk.js"
+  },
+  {
+    "revision": "5906521475a56438f543",
+    "url": "/marvel-memory-game/static/js/runtime~main.c25bf332.js"
+  }
+]);
